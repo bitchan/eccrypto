ae968011a8e855c35eab9abdd9179949afa8bad6
Kagami Hiiragi <kagami@genshiken.org>
2015-01-02 18:21:24 +0300

    Fix doc strings

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ var secp256k1 = require("secp256k1");
 
 /**
  * Compute the public key for a given private key.
- * @param {Buffer} publicKey A 32-byte private key
- * @return {Buffer} A 65-byte public key
+ * @param {Buffer} privateKey - A 32-byte private key
+ * @return {Buffer} A 65-byte public key.
  * @function
  */
 var getPublic = exports.getPublic = secp256k1.createPublicKey;
 
 /**
  * Create an ECDSA signature.
- * @param {Buffer} privateKey A 32-byte private key
- * @param {Buffer} msg The message being signed
- * @return {Promise.<Buffer,undefined>} A promise that resolves with the
- * signature or rejects on bad private key/message.
+ * @param {Buffer} privateKey - A 32-byte private key
+ * @param {Buffer} msg - The message being signed
+ * @return {Promise.<Buffer>} A promise that resolves with the
+ * signature and rejects on bad key or message.
  */
 // FIXME(Kagami): What to do in case of invalid nonce?
 exports.sign = function(privateKey, msg) {
@@ -42,11 +42,11 @@ exports.sign = function(privateKey, msg) {
 
 /**
  * Verify an ECDSA signature.
- * @param {Buffer} key Private or public key
- * @param {Buffer} msg The message being verified
- * @param {Buffer} sig The signature
- * @return {Promise} A promise that resolves on correct signature and
- * rejects on bad signature/public key.
+ * @param {Buffer} key - A private or public key
+ * @param {Buffer} msg - The message being verified
+ * @param {Buffer} sig - The signature
+ * @return {Promise.<undefined>} A promise that resolves on correct
+ * signature and rejects on bad key or signature.
  */
 exports.verify = function(key, msg, sig) {
   var publicKey = key.length === 32 ? getPublic(key) : key;
